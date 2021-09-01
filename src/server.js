97b69a414dc88742778d23c58c2c1bcc28bc34a4
on-stu <minsu0523@naver.com>
2021-09-01 17:25:59 +0900

    http and ws server working at the same time

--- src/server.js
+++ src/server.js
@@ -1,1 +1,19 @@
+import express from "express";
+import http from "http";
+import WebSocket from "ws";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
 console.log("hello");
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+server.listen(3000);
