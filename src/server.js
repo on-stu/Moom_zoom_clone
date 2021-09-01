6fea6282c3a89e0932d8cf6677801f0ab5a45ba3
on-stu <minsu0523@naver.com>
2021-09-01 21:47:06 +0900

    Web Socket Chat is done!

--- src/server.js
+++ src/server.js
@@ -10,10 +10,31 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-console.log("hello");
-
 const server = http.createServer(app);
 
 const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+function handleConnection(socket) {
+  sockets.push(socket);
+  socket["nickname"] = "anonymous";
+  socket.on("close", () => {
+    console.log("disconnected");
+  });
+
+  socket.on("message", (message) => {
+    const parsed = JSON.parse(message);
+    if (parsed.type === "new_message") {
+      sockets.forEach((aSocket) =>
+        aSocket.send(`${socket.nickname}: ${parsed.payload}`)
+      );
+    } else if (parsed.type === "nickname") {
+      socket["nickname"] = parsed.payload;
+    }
+  });
+}
+
+wss.on("connection", handleConnection);
 
 server.listen(3000);
